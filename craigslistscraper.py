5f14eacd47f1c1375a29c1f94a3cb8a31fa1dede
nadyac <nadyacpena@gmail.com>
2015-01-09 00:55:29 -0500

    added code so it only prints the price of the apartment listing

--- craigslistscraper.py
+++ craigslistscraper.py
@@ -1,5 +1,5 @@
-# clscraper1.py - rough web scraper that pulls posting titles from a given 
-# craigslist ad page. Need BeautifulSoup4 and requests installed to run. ***
+# webscraper.py - rough web scraper that pulls posting titles from a given 
+# craigslist ad page. 
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
 import bs4
@@ -8,8 +8,9 @@ bsObj = requests.get('http://newyork.craigslist.org/search/hhh?query=apartment&s
 soup = BeautifulSoup(bsObj, 'html.parser')
 
 # to get results from multiple classes:
-#results = soup.find_all('a', {'class' : ['i','hdrlnk']})
+# results = soup.find_all('a', {'class' : ['i','hdrlnk']})
 results = soup.find_all('a',{'class' : 'i'})
+
 
 for n in results:
 	lnk = 'http://newyork.craigslist.org' + n['href']
@@ -17,5 +18,12 @@ for n in results:
 	soup2 = BeautifulSoup(bsObj2, 'html.parser')
 	results2 = soup2.find_all('h2', {'class' : 'postingtitle'}) #returning empty list 
 	title = results2[0].text
+
+	# print apartment prices for the listings on current page
+	words = title.split()
+	for w in words:
+		if w.find('$') != -1:
+			print w
+
 
 
