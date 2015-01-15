40ffaa651c6d77ec5e160f3a8116e2cd59e46060
nadyac <nadyacpena@gmail.com>
2015-01-15 01:51:01 -0500

    Now gets the boat's price, the seller's contact number, and the make/model of the boat

--- statricksProj.py
+++ statricksProj.py
@@ -10,10 +10,25 @@ import bs4
 searchResults = requests.get('http://www.boattrader.com/search-results/').text
 soup = BeautifulSoup(searchResults, 'html.parser')
 
-# Get the link to the individual ad listing
-listing = soup.find('div', {'class' : 'ad-title'})
-listingLnk = listing.find('a')
-print listingLnk['href']
+# Get the links to each individual ad listings
+listing = soup.find_all('div', {'class' : 'ad-title'})
+for n in listing:
+	listingLnk = n.find('a')
+
+	#visit each ad listing to extract make/model
+	singleAd = requests.get('http://www.boattrader.com/' + listingLnk['href']).text
+	adSoup = BeautifulSoup(singleAd,'html.parser')
+	table = adSoup.find('div', {'class' : 'collapsible'})
+	tableItem = table.find_all('td')
+	make_model = tableItem[3].text # the make and model of the boat
+
+	# Extract the seller contact #
+	seller = adSoup.find('div', {'class' : 'phone'})
+	sellerNumber = seller.text # the seller's number
+
+	# Extract the Price of the boat
+	btPrice = adSoup.find('span', {'class' : 'bd-price'})
+	price = btPrice.text # the price of the boat
 
 # Create output file
 with open("output.csv", "wb") as csvfile:
